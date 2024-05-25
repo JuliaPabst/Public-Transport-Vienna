32acd9d95566cbc7f742e452933284eae36f98fb
petra-scheuer <151351918+petra-scheuer@users.noreply.github.com>
2024-05-25 21:00:51 +0200

    Update PathFinder.h
    added dijkstra

--- Program/PathFinder.h
+++ Program/PathFinder.h
@@ -5,13 +5,27 @@
 #ifndef PROGRAM_PATHFINDER_H
 #define PROGRAM_PATHFINDER_H
 
+#include "Graph.h"
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <queue>
 
 class PathFinder {
-    public:
+public:
+    PathFinder(Graph& graph, const std::string& start, const std::string& end);
+    void findShortestPath();
+    void printPath();
 
-    private:
+private:
+    Graph& graph_;
+    std::string start_;
+    std::string end_;
+    std::unordered_map<std::string, int> distances_;
+    std::unordered_map<std::string, std::string> previous_;
+    std::vector<std::string> path_;
 
+    void dijkstra();
 };
-
 
 #endif //PROGRAM_PATHFINDER_H
