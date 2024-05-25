de6f6d10b95244f38b11aa7743fae74f1f06b43d
petra-scheuer <151351918+petra-scheuer@users.noreply.github.com>
2024-05-25 21:00:04 +0200

    Update Graph.cpp

--- Program/Graph.cpp
+++ Program/Graph.cpp
@@ -1,5 +1,12 @@
 #include "Graph.h"
 #include <queue>
+std::vector<std::string> Graph::getNodes() const {
+    std::vector<std::string> nodes;
+    for (const auto& pair : adjList) {
+        nodes.push_back(pair.first);
+    }
+    return nodes;
+}
 
 void Graph::addEdge(const std::string& start, const std::string& end, int weight) {
     adjList[start].push_back(std::make_pair(end, weight));
