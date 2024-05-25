9de6c233ed722f8ac55bf5a504c462edab942506
petra-scheuer <151351918+petra-scheuer@users.noreply.github.com>
2024-05-25 20:59:41 +0200

    Update Graph.h

--- Program/Graph.h
+++ Program/Graph.h
@@ -7,6 +7,8 @@ class Graph {
     public:
         void addEdge(const std::string& start, const std::string& end, int weight);
         void printGraph();
+    const std::unordered_map<std::string, std::vector<std::pair<std::string, int>>>& getAdjList() const { return adjList; }
+    std::vector<std::string> getNodes() const;
     private:
         std::unordered_map<std::string, std::vector<std::pair<std::string, int>>> adjList;
 };
